2c9c2c7cc57472d7a80c546237795b4f2d121080
Juan Aguiar <jaguiarcruz17@gmail.com>
2026-01-19 21:33:13 -0500

    feat(auth): Add profile, logout routes and use auth guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Req,
   Res,
   UseGuards,
 } from '@nestjs/common';
@@ -11,15 +12,39 @@ import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/createUser.dto';
 import { LoginUser } from './dto/loginUser.dto';
 import type { Response } from 'express';
-import { AuthGuard } from './auth.guard';
+import { JwtAuthGuard } from './auth.guard';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtPayload } from './interface/jwtPayload.interface';
+
+interface RequestWithUser extends Request {
+  user: JwtPayload;
+}
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(AuthGuard)
+  @UseGuards(JwtAuthGuard)
+  @Post('profile')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Get user information' })
+  @ApiBody({})
+  @ApiResponse({
+    status: 200,
+    description: 'Get user information succesfully',
+    example: {
+      sub: 'user ID',
+      email: 'user email',
+      iat: 'token created and issued',
+      exp: 'invalid token',
+    },
+  })
+  getProfile(@Req() req: RequestWithUser) {
+    return req.user;
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
   @ApiOperation({ summary: 'Create a new user' })
@@ -48,13 +73,23 @@ export class AuthController {
     @Res({ passthrough: true }) response: Response,
     @Body() user: LoginUser,
   ) {
-    const token = await this.authService.signIn(user);
-    response.cookie('access_token', token, {
+    const { access_token, sub, email } = await this.authService.signIn(user);
+    response.cookie('access_token', access_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
       maxAge: 1000 * 60 * 60 * 24,
     });
-    return { token };
+    return {
+      sub,
+      email,
+    };
+  }
+
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'User logout' })
+  logout(@Res({ passthrough: true }) response: Response) {
+    response.clearCookie('access_token');
   }
 }
